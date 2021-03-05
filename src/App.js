bb317f79d14583a0d5797a6d9eb893383dcde052
Kay Richardson <73449592+KayRich-cmd@users.noreply.github.com>
2021-03-04 23:37:47 -0500

    Created Weather.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
@@ -5,6 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         RE-DO WEATHER APP! *REMOVE THIS WHEN DONE*
+        <Weather />
       </div>
     </div>
   );
