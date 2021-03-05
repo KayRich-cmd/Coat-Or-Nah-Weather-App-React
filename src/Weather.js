b30cb7a6bf37939bffe623c229ed7d9d0105dc90
Kay Richardson <73449592+KayRich-cmd@users.noreply.github.com>
2021-03-05 00:01:27 -0500

    Added Search Engine to Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,38 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Hello from Weather Component</div>;
+  return (
+    <div className="Weather">
+      <div className="card">
+        <div className="card-body">
+          <form>
+            <div className="row mt-2">
+              <div className="col">
+                <input
+                  type="search"
+                  className="form-control form-input-style"
+                  placeholder="Need A Coat Today? (Enter City Name)"
+                  autoFocus="on"
+                />
+              </div>
+              <div className="col-auto">
+                <a className="btn btn-secondary" href="/" role="button">
+                  Search
+                </a>
+              </div>
+              <div className="col-auto">
+                <a
+                  className="btn btn-secondary current-location-button"
+                  href="/"
+                  role="button"
+                >
+                  <i className="fas fa-map-marker-alt"></i>
+                </a>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
