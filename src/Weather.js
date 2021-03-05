3c138ab28962dea1958506b3f4e2fe6c76855320
Kay Richardson <73449592+KayRich-cmd@users.noreply.github.com>
2021-03-05 00:17:27 -0500

    Added Other Description Info to Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -59,6 +59,19 @@ export default function Weather() {
               <a href="/">C</a> |<a href="/"> F</a>
             </span>
           </div>
+
+          <div className="OtherDescription">
+            <ul>
+              <li>
+                <span className="other-description-label"> Humidity:</span>{" "}
+                <span>--%</span>
+              </li>
+              <li>
+                <span className="other-description-label"> Wind Speed:</span>{" "}
+                <span>--</span> km/h
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
