65ce9e0e14ba475055199eddae3029aa7311d952
Kay Richardson <73449592+KayRich-cmd@users.noreply.github.com>
2021-03-07 23:27:27 -0500

    Added an Animated Weather external component & created WeatherIcon.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -19,7 +20,7 @@ export default function WeatherInfo(props) {
       <div className="CurrentWeather">
         <div className="row">
           <div className="col-12">
-            <img src={props.data.iconUrl} alt={props.data.description} />
+            <WeatherIcon code={props.data.icon} alt={props.data.description} />
           </div>
         </div>
         <span>{Math.round(props.data.temperature)}°</span>
