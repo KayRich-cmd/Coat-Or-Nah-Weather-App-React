790ed2abed5d7eb3943589e9f8acace8c27e8b20
Kay Richardson <73449592+KayRich-cmd@users.noreply.github.com>
2021-03-19 00:44:28 -0400

    Updated TemperatureConversion.js
    The Celsius & Fahrenheit links are now working.

--- src/TemperatureConversion.js
+++ src/TemperatureConversion.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TemperatureConversion(props) {
-  return (
-    <div className="TemperatureConversion">
-      <span>{Math.round(props.celsius)}°</span>
-      <span className="unit-type">
-        <a href="/">C</a> |<a href="/"> F</a>
-      </span>
-    </div>
-  );
+  const [unit, setUnit] = useState("celsius");
+
+  function displayCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function displayFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="TemperatureConversion">
+        <span>{Math.round(props.celsius)}°</span>
+        <span className="unit-type">
+          <span className="active">C</span> |
+          <a href="/" onClick={displayFahrenheit}>
+            {" "}
+            F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="TemperatureConversion">
+        <span>{Math.round(fahrenheit())}°</span>
+        <span className="unit-type">
+          <a href="/" onClick={displayCelsius}>
+            {" "}
+            C
+          </a>{" "}
+          |<span className="active"> F</span>
+        </span>
+      </div>
+    );
+  }
 }
